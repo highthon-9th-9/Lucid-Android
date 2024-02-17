d2af2ef2bd2195b77fb4db05ff5cb87419d7162d
uielPark <parkuiery@gmail.com>
2024-02-17 17:36:01 +0900

    feat :: LucidApp

---
+++ app/src/main/java/com/example/lucid/LucidApp.kt
@@ -0,0 +1,21 @@
+package com.example.lucid
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.lucid.feature.login.LoginScreen
+
+@Composable
+fun LucidApp() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "login",
+    ) {
+        composable("login") {
+            LoginScreen()
+        }
+    }
+}
